3f3846b601041491f3b15f41c3f4c768b78e0847
chihoxtra <samuelpun@gmail.com>
2015-07-20 17:36:28 +0800

    testing version 1

--- GearRatio/ViewController.swift
+++ GearRatio/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var chainNumberTextField: UITextField!
     @IBOutlet weak var cogNumberTextField: UITextField!
     @IBOutlet weak var gearRatioLabel: UILabel!
+    @IBOutlet weak var skidPatchesLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,21 +31,52 @@ class ViewController: UIViewController {
         return toDouble
     }
     
-
-    @IBAction func calculateButton(sender: AnyObject) {
-        var ratio: Double = 0.0, cr: Double, cg: Double, tstr:String
+    
+    func simplifyNumbers(c1: Double, c2: Double) -> (Double, Double) {
         
+        var tmp1 = c1 , tmp2 = c2
+        
+        for i in 1...10 {
+            var pn: Double
+            pn = Double(10-i)
+            
+            if (remainder(tmp1, pn) == 0) && (remainder(tmp2, pn) == 0) {
+                tmp1 = tmp1/pn
+                tmp2 = tmp2/pn
+            }
+            
+        }
+        return (tmp1, tmp2)
+    }
+    
+
+    // handling the "actions" after pressing the button
+    @IBAction func calculateButton(sender: AnyObject) {
+        var ratio: Double = 0.0
+        var cr: Double = 0.0
+        var cg: Double = 0.0
+        var tmp_str: String, tmp_str2: String
+        
+        //content in the TextFiekds are strings and have to be converted first b4 calculation
+        //converting to from Strings to double
         cr = stringToDouble(chainNumberTextField.text)
         cg = stringToDouble(cogNumberTextField.text)
         
+        
+        
         ratio = cr/cg
         
-        tstr = NSString(format: "%.2f", ratio) as String
+        // Double to String
+        tmp_str = NSString(format: "%.2f", ratio) as String
+        tmp_str2 = NSString(format: "%.2f", simplifyNumbers(cr, c2: cg).1) as String
         
         chainNumberTextField.resignFirstResponder()
         cogNumberTextField.resignFirstResponder()
         
-        gearRatioLabel.text = tstr
+        gearRatioLabel.text = "Gear Ratio: " + tmp_str
+        skidPatchesLabel.text = "No. of Skid Patches: " + tmp_str2
+        
+        skidPatchesLabel.hidden = false
         gearRatioLabel.hidden = false
         
     }
